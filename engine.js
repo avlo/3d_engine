8ba2efc524c80e0ba81e9a40052dc965c668eb6c
nick avlo <avlo@yahoo.com>
2026-04-10 20:53:57 +0800

    up

--- engine.js
+++ engine.js
@@ -6,11 +6,12 @@ console.log(ctx)
 
 const BACKGROUND = "#101010"
 const VERTICES_FOREGROUND = "#11FF50"
+const VERTICES_TEXT = "#996666"
 const LINES_FOREGROUND = "#FFFF50"
 const FPS = 600;
 let dt_fps = 1 / FPS;
 
-const point_pixels_width = 10
+const point_pixels_width = 1
 const line_pixels_width = 1
 const point_half = point_pixels_width / 2
 
@@ -27,13 +28,13 @@ function draw_point({x, y}, pixels_width, foreground) {
   ctx.fillRect(x, y, width, width)
 }
 
-function add_text(text_height, x, y, foreground) {
-  ctx.strokeStyle = foreground
+function add_text(text, text_height, x, y, foreground) {
+  ctx.fillStyle = foreground
   let heightPx = text_height / 2
   ctx.font = heightPx + "px verdana";
 
-  // ctx.fillText("test", p1.x, p1.y, maxWidth)
-  ctx.strokeText("test", x, y, heightPx)
+  ctx.fillText(text, x, y, heightPx)
+  // ctx.strokeText("test", x, y, heightPx)
   // ctx.fillText("x: "+ ~~(p1.x-(p1.x*.88)), 2, 50, 300)
   // ctx.fillText("y: "+ ~~p1.y, 2, 100, 300)  
 }
@@ -77,7 +78,7 @@ function canvasIze(normalized_point) {
 
 // translate point (x,y) from screen center coordinates (0, 0) to HTML canvas top left coordinates (0, w/h), i.e,
 //    -1..1 => 0..w/h
-function centeredCoordinatesToCanvasCoordinates(centered_point) {
+function convertCenteredCoordinatesToCanvasCoordinates(centered_point) {
 // (1of3) translate negative coord to positive coord
   let positivized_point = positivize(centered_point);
 // (2of3) divide by 2 normalizes the result
@@ -113,19 +114,16 @@ function translate({x, y, z}, dz) {
   return {x, y, z: z + 2}
 }
 
-let dz = 0;
-let theta = 0
-
 function draw_rotating_lines(dz, theta, vertices, lines) {
   for (const line of lines) {
     for (let i = 0; i < line.length; i++) {
       const start = vertices[line[i]] // first vertex
       const end = vertices[line[(i + 1) % line.length]] // % == last vertex wrap around 
-      let p1 = centeredCoordinatesToCanvasCoordinates(
+      let p1 = convertCenteredCoordinatesToCanvasCoordinates(
           project_3d_to_2d(
               translate(
                   rotate(start, theta), dz)));
-      let p2 = centeredCoordinatesToCanvasCoordinates(
+      let p2 = convertCenteredCoordinatesToCanvasCoordinates(
           project_3d_to_2d(
               translate(
                   rotate(end, theta), dz)));
@@ -136,21 +134,26 @@ function draw_rotating_lines(dz, theta, vertices, lines) {
 
 function draw_rotating_vertices(dz, theta, vertices) {
   for (const vertex of vertices) {
-    let point = centeredCoordinatesToCanvasCoordinates(
+    let point = convertCenteredCoordinatesToCanvasCoordinates(
         project_3d_to_2d(
             translate(
                 rotate(vertex, theta), dz)));
-    draw_point(point, point_pixels_width, VERTICES_FOREGROUND)
-    add_text(100, point.x, point.y, VERTICES_FOREGROUND)
+
+    let decrement = point.x <= canvas.width / 2
+    let color = decrement ? VERTICES_FOREGROUND : VERTICES_TEXT
+    let text = decrement ? "+" : "-"
+    draw_point(point, point_pixels_width*10, VERTICES_FOREGROUND)
+    add_text(text, 100, point.x, point.y, color)
   }
 }
 
 function draw_some_lines(lines, line_width) {
   // clear()
   let colors = [VERTICES_FOREGROUND, LINES_FOREGROUND]
-  let i = 0
+  let j = 0
   for (const line of lines) {
-    let color = colors[i++ % 2]
+    // let color = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0")
+    let color = colors[j++ % 2]
     for (let i = 0; i < line.length; i++) {
       draw_line({
         x: line[i],
@@ -170,14 +173,42 @@ function draw_some_lines(lines, line_width) {
   }
 }
 
+let dz = 0;
+let theta = 0
+
+function display_theta(theta) {
+  let theta_precision = Math.cos(theta).toPrecision(2);
+  let decrement = theta_precision <= 0
+  let color = decrement ? VERTICES_FOREGROUND : VERTICES_TEXT
+  add_text("θ =", 70, 700, 50, VERTICES_FOREGROUND)
+  add_text(theta_precision, 85, 740, 50, color)
+}
+
 function main() {
   dz += dt_fps
   theta += 2 * Math.PI * dt_fps // rotation speed
   clear()
-  draw_some_lines(data_single_lines, point_pixels_width/10)
+  display_theta(theta);
+  draw_some_lines(generateRandomLines(10),point_pixels_width/5)
+  draw_some_lines(data_single_lines, point_pixels_width)
   draw_rotating_vertices(dz, theta, data_vertices);
   draw_rotating_lines(dz, theta, data_vertices, data_lines);
   setTimeout(main, 500 / FPS)
+}
+
+function generateRandomLines(numlines) {
+  let array = [[]]
+  for (let i = 0; i < numlines; i++) {
+    let p1x = canvas.width - (canvas.width * Math.random())
+    let p1y = canvas.height - (canvas.height * Math.random())
+    let p2x = canvas.width - (canvas.width * Math.random())
+    let p2y = canvas.height - (canvas.height * Math.random())
+    let internal_array = [p1x, p1y, p2x, p2y]
+    array.push(internal_array)
+  }
+  // let values = data_single_lines.values();
+  // values.forEach(value => array.push(value))
+  return array
 }
 
 const data_single_lines = [
